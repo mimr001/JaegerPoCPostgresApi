d96465c3d32ddde47c3322ec3dd1ef053051d6f3
Michael Mrazek <michael.mrazek@gmail.com>
2017-09-28 12:25:09 +0200

    Switching to node:alpine in Dockerfile:FROM. Added env variables for Jaeger UDP host/port.

--- server.js
+++ server.js
@@ -7,14 +7,17 @@ const Instrument = require('@risingstack/opentracing-auto')
 // Jaeger tracer (standard distributed tracing)
 const jaeger = require('jaeger-client')
 const UDPSender = require('jaeger-client/dist/src/reporters/udp_sender').default
-const sampler = new jaeger.RateLimitingSampler(1)
-// Need this since the Jaeger server parts (reporter, collector, storage etc) are running outside the scope of our
-// Docker stack in this PoC. Real case scenario, the Jaeger server parts will either run in the same
-// Docker stack or in a separate Docker stack but on the same host to avoid network latency to the reporter
+const sampler = new jaeger.RateLimitingSampler(10)
+// Need this since the Jaeger server parts (reporter, collector, storage etc) are running outside the scope of
+// our Docker stack in this PoC. Real case scenario, the Jaeger server parts will either run in the same
+// Docker stack or in a separate Docker stack but on the same host to avoid network latency
 const reporter = new jaeger.RemoteReporter(new UDPSender({
+  // host: 'ec2-54-93-196-139.eu-central-1.compute.amazonaws.com', // Directly on EC2 Node for now...
   // host: 'docker.for.mac.localhost',
-  host: 'localhost',
-  port: 6832
+  // host: 'localhost',
+  // port: 6832
+  host: process.env.JAEGER_AGENT_UDP_HOST,
+  port: process.env.JAEGER_AGENT_UDP_PORT
 }))
 const jaegerTracer = new jaeger.Tracer('jaeger-poc-postgresapi-jaeger-tracer', reporter, sampler)
 
@@ -36,10 +39,13 @@ const http = require('http')
 // const massive = require('massive')
 const knex = require('knex')
 
+console.log("Jaeger UDP host:" + process.env.JAEGER_AGENT_UDP_HOST)
+console.log("Jaeger UDP port:" + process.env.JAEGER_AGENT_UDP_PORT)
+
 const db = knex({
   client: 'pg',
-  connection: 'postgres://postgres:@localhost:5432/postgres'
-  // connection: 'postgres://postgres:@postgres:5432/postgres'
+  // connection: 'postgres://postgres:@localhost:5432/postgres'
+  connection: 'postgres://postgres:@postgres:5432/postgres'
 })
 
 var app = express();
